b94ba940ca02c9482dc42b1fc2e3f8728cecd543
Adriano Almeida <adriano@z1.app>
2022-05-12 22:49:25 -0300

    added get and put e-mails dynamodb

--- src/functions/registerEmail.ts
+++ src/functions/registerEmail.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
+import { document } from "../utils/dynamodbClient"
 
 interface IRegisterEmail {
   id: string;
@@ -10,7 +11,27 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   const { id, name, email } = JSON.parse(event.body) as IRegisterEmail
 
-  console.log(email, id, name)
+  const response = await document.query({
+    TableName: "email_list",
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+        ":id": id
+    }
+  }).promise();
+
+  const emailAlreadyExists = response.Items[0];
+
+  if (!emailAlreadyExists) {
+    await document.put({
+        TableName: "email_list",
+        Item: {
+            id,
+            name,
+            email,
+            created_at: new Date().getTime()
+        }
+    }).promise();
+  }
   
   return {
       statusCode: 201,
